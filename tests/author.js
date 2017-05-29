cd5af8f33a7a4e41a3fc58087e5907e8994be98a
Mikhail Subach <scorpibear@tut.by>
2017-05-29 19:39:10 +0300

    issue #3 closed - author

---
+++ tests/author.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'Author specified as "автор Миша Субоч"': function(browser) {
+    let pathToArticle = "2012/12/new-year-new-goal/";
+    browser.url(browser.launchUrl + pathToArticle);
+    browser.expect.element('.entry-meta').text.to.contain('АВТОР МИША СУБОЧ');
+    browser.end();
+  }
+};
