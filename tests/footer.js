a5d8833b94224c29d31e8f025ec0753059c59fec
Mikhail Subach <scorpibear@tut.by>
2017-08-16 19:41:11 +0300

    Close #7: Meta section is not displayed

--- tests/footer.js
+++ tests/footer.js
@@ -5,5 +5,10 @@ module.exports = {
       .url(browser.launchUrl)
       .assert.containsText('footer', `LifeIdea.org © ${currentYear}`)
       .end();
+  },
+  'META section is not displayed': function(browser) {
+    browser.url(browser.launchUrl);
+    browser.expect.element('footer').text.to.not.contain('МЕТА');
+    browser.end();
   }
 };
